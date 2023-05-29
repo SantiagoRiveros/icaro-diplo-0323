3935ece9a6047a9aca3d25dfc5d1a00ffdfd1f79
Santiago Riveros Jasis <70020219+SantiagoRiveros@users.noreply.github.com>
2023-05-29 18:14:20 -0300

    Create clase1.js

---
+++ clase1.js
@@ -0,0 +1,46 @@
+//archivo de prueba
+//poner texto descriptivo
+
+// variable esUnNumero
+var esUnNumero = 2; //NUMBER
+// esUnNumero camelcase
+// EsUnNumero pascalCase
+// es_un_numero snakeCase
+// es-un-numero
+var esOtroNumero = 3;
+
+// variable esUnaOracion
+var esUnaOracion = "Hola Mundo!"; //STRING
+// variable esVerdadero
+var esVerdadero = true; //BOOLEAN
+// true es verdadero
+// false es falso
+
+console.log(esUnaOracion);
+console.log("hola");
+
+//paso 1
+console.log(1 < 0);
+//paso 2
+console.log(false == true);
+//paso 3
+console.log(1 + "1");
+//queres que te llegue 1 y te llega 4
+
+//if y else
+// if(condicion){}
+//
+if (esUnNumero < esOtroNumero) {
+  console.log("mirenme, estoy identado");
+  console.log("es menor");
+} else {
+  console.log("es mayor");
+}
+
+var esVerdad = false;
+
+if (esVerdad) {
+  console.log("es Verdad!!!");
+} else {
+  console.log("no es verdad :(");
+}
